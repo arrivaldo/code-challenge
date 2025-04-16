95b85356d33aef082fefbe3c79a26b602b96fee8
arrivaldo <erickbarcena00@hotmail.com>
2025-04-15 22:39:11 -0600

    i

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Signup/>} />
+          <Route path="/" element={<Login/>} />
           <Route path="/register" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           
